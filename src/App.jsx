99501e074492b188ce1570e27c9c9e5672615cf0
chitra0404 <chitrasuresh0404@gmail.com>
2024-02-05 16:56:02 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
       <Route path="/" element={<Dashboard/>}></Route> 
       <Route path="/register" element={<Register/>}></Route>
         <Route path="/user/register" element={<Regis/>}></Route>
-        <Route path="/user/accactivation/:id" element={<Account/>}></Route> 
+        <Route path="/user/accactivation/:id" element={<AccountActivation/>}></Route> 
         <Route path="/users/login" element={<Log/>}></Route>
         <Route path="/staf" element={<StaffDashboard/>}></Route>
    
@@ -52,7 +52,7 @@ function App() {
         <Route path="/emp" element={<GetEmp/>}></Route>
         <Route path="/contact" element={<Contact/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
-        <Route path="/accountactivation/:id" element={<AccountActivation/>}></Route>
+        <Route path="/accountactivation/:id" element={<Account/>}></Route>
   
         
 
