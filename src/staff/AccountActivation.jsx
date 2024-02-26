91ceeccd1848e4c539bfa36c99f683fc51eb9d4b
chitra0404 <chitrasuresh0404@gmail.com>
2024-02-26 14:27:12 +0530

    com

--- src/staff/AccountActivation.jsx
+++ src/staff/AccountActivation.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { Base_Url } from "../../config/api";
@@ -10,10 +10,25 @@ function AccActivation() {
   const [activated, setActivated] = useState(false);
   const [done, setDone] = useState(false);
 
-  const HandleActivate = async (id) => {
+  useEffect(() => {
+    const checkAccountActivation = async () => {
+      try {
+        // Make an API call to check if the account is already activated
+        const response = await axios.get(`${Base_Url}/user/check-activation/${id}`);
+        const { activated } = response.data;
+        setActivated(activated);
+      } catch (err) {
+        console.error(err);
+        setDone(true);
+      }
+    };
+
+    checkAccountActivation();
+  }, [id]);
+
+  const handleActivate = async (id) => {
     try {
-        await axios.patch(`${Base_Url}/user/activate/${id}`)
-      .then(res=>console.log(res));
+      await axios.patch(`${Base_Url}/user/activate/${id}`);
       setActivated(true);
       setTimeout(() => {
         Navigate("/");
@@ -33,26 +48,31 @@ function AccActivation() {
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col col-xl-10">
             <div
-              className="card  text-center fw-bold"
+              className="card text-center fw-bold"
               style={{ borderRadius: "1rem", height: "10vw", fontSize: "3vw" }}
             >
               <div className="row d-flex justify-content-center align-items-center">
                 <div className="col-lg-6 col-sm-12">
-                  <Link>
-                    <button
-                      className="btn btn-primary"
-                      onClick={() => HandleActivate(id)}
-                    >
-                      Click Me to Activate
-                    </button>
-                  </Link>
+                  {!activated && (
+                    <Link>
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => handleActivate(id)}
+                      >
+                        Click Me to Activate
+                      </button>
+                    </Link>
+                  )}
                 </div>
               </div>
 
               <p>
                 <span>
-                  {activated ? "Account Activated Successfully" : null}
-                  <span>{done ? "Activated Account" : null}</span>
+                  {activated
+                    ? "Account Activated Successfully"
+                    : done
+                    ? "Activated Account"
+                    : null}
                 </span>
               </p>
             </div>
